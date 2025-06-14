5eaae08dfa1145ca8ac232ed8f668e24140cdcba
tawandakasunzuma <tskasunzuma@gmail.com>
2025-06-14 22:54:53 +0200

    correct icons src in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,8 @@ export default defineConfig({
         short_name: 'SearchaGif',
         name: 'Searcha Gif',
         icons: [
-          { src: './icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: './icons/icon-512x512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' }
         ],
         start_url: '.',
         display: 'standalone',
